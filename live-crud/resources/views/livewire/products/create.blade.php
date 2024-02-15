b4e15d1a375eb1eb97c57a5391c585522d75afe3
Riaz Hassan <rhassan70@gmail.com>
2024-02-15 11:20:41 +0500

    qq

--- live-crud/resources/views/livewire/products/create.blade.php
+++ live-crud/resources/views/livewire/products/create.blade.php
@@ -1,3 +1,36 @@
-<div>
-hello
+<div class="modal fade" wire:ignore.self id="createModal" tabindex="-1" role="dialog" aria-labelledby="createModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="createModalLabel">Add Product</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" wire:model="name" placeholder="Enter name">
+                        @error('name') <span class="text-danger">{{ $message }}</span>@enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Description</label>
+                        <textarea class="form-control" id="description" wire:model="description" placeholder="Enter description"></textarea>
+                        @error('description') <span class="text-danger">{{ $message }}</span>@enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="price">Price</label>
+                        <input type="text" class="form-control" id="price" wire:model="price" placeholder="Enter price">
+                        @error('price') <span class="text-danger">{{ $message }}</span>@enderror
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button wire:click.prevent="store()" type="button" class="btn btn-primary">Save</button>
+            </div>
+        </div>
+    </div>
 </div>
+
